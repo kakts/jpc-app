37d8a5959699e6962c1264aed99ee4ee592760b5
kakts <kakts.git@gmail.com>
2025-02-05 00:40:23 +0900

    [TipTime] add Tip calculator. with round up tip flag

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.example.tiptime
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -28,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,7 +65,20 @@ fun TipTimeLayout() {
     // ?: はエルビス演算子
     val amount = amountInput.toDoubleOrNull() ?: 0.0;
 
-    val tip = calculateTip(amount)
+
+    /**
+     * カスタムチップ率用のフィールド
+     * 再コンポーズしても値を保持する
+     */
+    var tipInput by remember { mutableStateOf("")}
+
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+
+    // チップの丸め計算フラグ用のスイッチの状態
+    var roundUp by remember { mutableStateOf(false)}
+
+    val tip = calculateTip(amount, tipPercent, roundUp)
+
 
     Column(
         modifier = Modifier
@@ -78,10 +96,31 @@ fun TipTimeLayout() {
                 .align(alignment = Alignment.Start)
         )
         EditNumberField(
+            label = R.string.bill_amount,
             value = amountInput,
             onValueChange = { amountInput = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next // Nextの場合ボタンを押すと次のフィールドに移動する
+            ),
             modifier = Modifier
                 .padding(bottom = 32.dp).fillMaxWidth()
+        )
+        // カスタムチップ率用のフィールド
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            value = tipInput,
+            onValueChange = { tipInput = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done // キーボードの入力が完了する
+            ),
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
+        )
+        RoundTheTipRow(
+            roundUp = roundUp,
+            onRoundUpChanged = { roundUp = it },
+            modifier = Modifier.padding(bottom = 32.dp)
         )
         Text(
             text = stringResource(R.string.tip_amount, tip),
@@ -93,9 +132,14 @@ fun TipTimeLayout() {
 
 /**
  * 入力フォーム
+ *
+ * 引数に@StringResアノテーションを指定することにより、
+ * その引数が文字列リソース参照であることを示す
  */
 @Composable
 fun EditNumberField(
+    @StringRes label: Int,
+    keyboardOptions: KeyboardOptions,
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier) {
@@ -105,18 +149,53 @@ fun EditNumberField(
         value = value,
         onValueChange = onValueChange,
         singleLine = true,
-        label = { Text(stringResource(R.string.bill_amount)) },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        modifier = Modifier.fillMaxWidth()
+        label = { Text(stringResource(label)) },
+        keyboardOptions = keyboardOptions,
+        modifier = modifier
     )
 
 }
 
 /**
+ * チップの丸め計算するかのフラグスイッチボタン
+ */
+@Composable
+fun RoundTheTipRow(
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier) {
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = stringResource(R.string.round_up_tip))
+        Switch(
+            checked = roundUp, // スイッチがオンになっているか
+            onCheckedChange = onRoundUpChanged, // スイッチがクリックされた時のコールバック
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End)
+        )
+    }
+}
+
+
+/**
  * チップの計算を行う
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0,
+    roundUp: Boolean): String {
+    var tip = tipPercent / 100 * amount
+
+    // 小数丸め計算する場合 四捨五入する
+    if (roundUp) {
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
